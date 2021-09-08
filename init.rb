056e66dda16c79d041c206a1fe1b0829c61f0ae3
Mauro Amico <mauro.amico@unibo.it>
2021-09-08 17:49:53 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ Rails.logger.info 'Starting Auto Correlation plugin...'
 
 require_dependency 'redmineautocorrelation_issue_change_listener'
 
-Redmine::Plugin.register :auto_correlation do
+Redmine::Plugin.register :redmine_auto_correlation do
   name 'Auto Correlation plugin'
   author 'Mauro Amico'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'https://github.com/mamico/RedmineAutoCorellationPlugin'
+  url 'https://github.com/unibo/redmine_auto_correlation'
   author_url 'https://github.com/mamico'
 end
